0eba105af33970f4d3926bc9481aa8a31070e138
MidnightDev024 <chawlaakash024@gmail.com>
2025-09-17 22:19:46 +0530

    feat: add Navbar component and integrate it into the Home page

--- frontend/my-portfolio/src/app/page.tsx
+++ frontend/my-portfolio/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/hero";
+import Navbar from "./components/navbar";
 
 export default function Home() {
-  return (<>
-  <Hero />
-   
-  </>);
-}
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+};
