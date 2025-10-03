0945db6ad1cb5a06b74c75a6e143f83094bf1ca1
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-03 16:21:41 +0530

    refactored: projectCard.tsx

--- frontend/my-portfolio/src/app/components/projectcard.tsx
+++ frontend/my-portfolio/src/app/components/projectcard.tsx
@@ -27,6 +27,7 @@ const DeconProjectCard: React.FC<DeconProjectCardProps> = ({ project, onClick })
       <div className="p-6">
         <h3 className="text-xl font-bold text-white">{project.title}</h3>
         <p className="text-slate-400 mt-2">{project.description}</p>
+        <a href="https://text-utils-kappa-ten.vercel.app/"></a>
       </div>
     </div>
   );
