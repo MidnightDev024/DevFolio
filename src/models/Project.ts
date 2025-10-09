25a9f8d1ccc9057756e2a3c1e764a3c2d2e7061a
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-09 21:56:41 +0530

    refactored: project.ts

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,47 +1,45 @@
 import mongoose, { Schema, models, Document } from 'mongoose';
 
-export interface IProjectComponentPosition {
-  x: number;
-  y: number;
-}
-
-export interface IProjectComponent {
-  id: string;
-  name: string;
-  annotation: string;
-  image: string;
-  size: 'small' | 'large';
-  position: IProjectComponentPosition;
-}
-
 export interface IProject extends Document {
   title: string;
   description: string;
-  mainImage: string;
-  components?: IProjectComponent[];
+  mainImage: string;  
+  liveLink?: string;
+  tags: string[];
+  features: string[];
+  components: {
+    id: string;
+    name: string;
+    image: string;
+    annotation: string;
+    position: { x: number; y: number };
+    size?: string;
+  }[];
 }
-
-const ProjectComponentPositionSchema = new Schema<IProjectComponentPosition>({
-  x: { type: Number, required: true },
-  y: { type: Number, required: true },
-}, { _id: false });
-
-const ProjectComponentSchema = new Schema<IProjectComponent>({
-  id: { type: String, required: true },
-  name: { type: String, required: true },
-  annotation: { type: String, required: true },
-  image: { type: String, required: true },
-  size: { type: String, enum: ['small', 'large'], required: true },
-  position: { type: ProjectComponentPositionSchema, required: true },
-}, { _id: false });
 
 const ProjectSchema = new Schema<IProject>({
   title: { type: String, required: true },
   description: { type: String, required: true },
   mainImage: { type: String, required: true },
-  components: { type: [ProjectComponentSchema], default: [] },
+  liveLink: String,
+  tags: [String],
+  features: [String],
+  components: {
+    type: [{
+      _id: false,
+      id: String,
+      name: String,
+      image: String,
+      annotation: String,
+      size: String,
+      position: {
+        type: { x: Number, y: Number },
+        _id: false,
+      },
+    }],
+    default: [],
+  },
 }, {
-  // This is important: it tells Mongoose to use the 'projects' collection
   collection: 'projects' 
 });
 
