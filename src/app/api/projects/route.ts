e0ea3be24b9cf42723c2f3bea8d19ff5a3577c58
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-27 13:17:10 +0530

    refactored : route.ts

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect';
 import Project from '@/models/Project';
 
-export async function GET(request: Request) {
+export async function GET() {
   await dbConnect();
   try {
-    const projects = await Project.find({});
+    const projects = await Project.find({}).lean();
     return NextResponse.json({ success: true, data: projects }, { status: 200 });
   } catch (error) {
-    console.error("API Find Error:", error);
+    console.error('API Find Error:', error);
     return NextResponse.json({ success: false, error: 'Server Error' }, { status: 500 });
   }
 }
