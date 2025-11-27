3afe2e0d979a4e1e88c7184779a7a890d94f3b7d
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-27 13:48:29 +0530

    refactored : projects.ts

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -12,9 +12,9 @@ const projects = [
         name: "Navbar",
         image: "/textutilsNavBar.png",
         annotation: "Designed a clean, responsive navigation with user authentication status.",
-        position: { x: -80, y: -100 },
-        mobilePosition: { x: 0, y: -80 },
-        size: 'large'
+        position: { x: -85, y: -100 },
+        mobilePosition: { x: 0, y: -50 },
+        size: 'Large'
       },
       {
         id: "c1-gallery",
@@ -36,34 +36,34 @@ const projects = [
   },
   {
     id: 2,
-    title: "NewsHatch - A app for latest news",
-    description: "A news app that provides the latest news articles from various sources. created with the concept of fetching data from an API.",
-    mainImage: "/NewsHatch.png",
-    liveLink: "https://news-hatch.vercel.app/",
-    previewLink: "https://news-hatch.vercel.app/",
+    title: "AuraChat - Connect Your Frequency",
+    description: "A real-time chat application that allows users to connect and communicate seamlessly. Allow Users to Connect with their loved ones.",
+    mainImage: "/AuraChat_MainPage.png",
+    liveLink: "https://aura-chat-ten.vercel.app",
+    previewLink: "https://aura-chat-ten.vercel.app",
     components: [
       {
         id: "c1-navbar",
-        name: "Navbar",
-        image: "/newHatchNavBar.png",
-        annotation: "Designed a clean, responsive navigation with user authentication status.",
-        position: { x: -80, y: -100 },
+        name: "SignUp",
+        image: "/AuraChat_SignUp.png",
+        annotation: "Authentication and user login interface.",
+        position: { x: -85, y: -100 },
         mobilePosition: { x: 0, y: -80 },
-        size: 'large'
+        size: 'medium'
       },
       {
         id: "c1-gallery",
-        name: "Main Section",
-        image: "/newhatchHero.png",
-        annotation: "Displays the latest news articles with a clean and modern layout.",
+        name: "Chat Interface",
+        image: "/AuraChat_HomePage.png",
+        annotation: "Displays the chat interface with real-time message updates.",
         position: { x: -40, y: 10 },
         mobilePosition: { x: -30, y: 0 },
       },
       {
         id: "c1-form",
         name: "About Section",
-        image: "/newhatchBotomLook.png",
-        annotation: "Further look at APP features. Include Previous and Next buttons for better navigation.",
+        image: "/AuraChat_ProfilePage.png",
+        annotation: "User profile management and settings.",
         position: { x: 60, y: -60 },
         mobilePosition: { x: 30, y: 80 },
       },
